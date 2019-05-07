ee362f24b1420945e984cd976a6bf531beec1731
unknown <chrsitw@g.clemson.edu>
2019-05-06 20:53:00 -0400

    Add Difficulty Enum

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -16,7 +16,9 @@ public class Recipe {
     private Integer servings;
     private String url;
     private String directions;
-//    private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -98,5 +100,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
